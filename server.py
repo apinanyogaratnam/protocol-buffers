df21a916fa0995200ed5d4ee44d5f58575bf96db
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 00:12:14 -0400

    feat: created server

---
+++ server.py
@@ -0,0 +1,22 @@
+import grpc
+import square_root_pb2
+import square_root_pb2_grpc
+from concurrent import futures
+
+
+class SquareRootServiceServicer(square_root_pb2_grpc.SquareRootServiceServicer):
+    def SquareRoot(self, request, context):
+        return super().SquareRoot(request, context)
+
+
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    print("Starting server. Listening on port 50051.")
+    square_root_pb2_grpc.add_SquareRootServiceServicer_to_server(
+        SquareRootServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+main()
