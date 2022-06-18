45f80ab718ab9d677f5ccc50ab42396afa1d58bf
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 00:15:52 -0400

    fix: using more appropriate input value

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import square_root_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = square_root_pb2_grpc.SquareRootServiceStub(channel)
-        response = stub.SquareRoot(square_root_pb2.Number(input=7))
+        response = stub.SquareRoot(square_root_pb2.Number(input=49))
         print("Response: ", response.number)
 
 
