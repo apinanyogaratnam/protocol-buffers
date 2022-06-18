4a70869dc9e973390c0d5733c56a4e7fc64f2b0e
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 00:13:08 -0400

    fix: client

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import square_root_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = square_root_pb2_grpc.SquareRootServiceStub(channel)
-        response = stub.squareRoot(square_root_pb2.Number(input=7))
+        response = stub.SquareRoot(square_root_pb2.Number(input=7))
         print("Response: ", response.number)
 
 
