2897ad8a343d5c973b6abfdfa264db8cf1025bee
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 00:12:07 -0400

    feat: created client

---
+++ client.py
@@ -0,0 +1,14 @@
+import grpc
+import square_root_pb2
+import square_root_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = square_root_pb2_grpc.SquareRootServiceStub(channel)
+        response = stub.squareRoot(square_root_pb2.Number(input=7))
+        print("Response: ", response.number)
+
+
+if __name__ == '__main__':
+    run()
